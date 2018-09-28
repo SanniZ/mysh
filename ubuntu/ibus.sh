49e7eba537e8297eac598cc5bc4dc8b690889b64
yingbinx <yingbinx.zeng@intel.com>
2018-09-28 13:51:16 +0800

    add function to setup ibus input

--- ubuntu/ibus.sh
+++ ubuntu/ibus.sh
@@ -4,10 +4,20 @@ function help_menu()
 {
     echo '-r'
     echo '  restart'
+    echo '-L'
+    echo '  list engine'
     echo '-s'
     echo '  setup sunpinyin'
     echo '-p'
     echo '  setup pinyin'
+    echo '-g'
+    echo '  setup googlepinyin'
+    echo '-w'
+    echo '  setup wubi98'
+    echo '-v'
+    echo '  setup wubi-haifeng86'
+    echo '-V'
+    echo '  setup wubi-jidian86'
 }
 
 function ibus_restart()
@@ -17,25 +27,38 @@ function ibus_restart()
 
 function ibus_setup()
 {
-    if [ $1 == 'sunpinyin' ]; then
-        ibus engine sunpinyin
-    else
-        echo 'unknow, set default: sunpinyin'
-        ibus engine sunpinyin
-    fi
+    ibus engine $1
 }
 
 if [ $# == 0 ]; then
     help_menu
 else
-    while getopts 'rsh' opt
+    while getopts 'rLspgwvVh' opt
     do
         case ${opt} in
         r):
             ibus_restart
         ;;
+        L):
+            ibus list-engine
+        ;;
         s):
             ibus_setup 'sunpinyin'
+        ;;
+        p):
+            ibus_setup 'pinyin'
+        ;;
+        g):
+            ibus_setup 'googlepinyin'
+        ;;
+        w):
+            ibus_setup 'wubi98'
+        ;;
+        v):
+            ibus_setup 'wubi-haifeng86'
+        ;;
+        V):
+            ibus_setup 'wubi-jidian86'
         ;;
         h):
             help_menu
