fbebe81d1874488a4780680631d55c386afe98b9
Byng Zeng <sanni230@126.com>
2019-11-19 13:04:01 +0800

    :update mygit.sh
    1. create git.sh.
    2. move mygit.sh to git/mygit.sh and updated.

---
+++ git/git.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+
+VERSION='1.0.0'
+
+CURDIR=$(pwd)
+
+
+function git_help()
+{
+    if [ $# -gt 0 ]; then
+        echo "---------------------------------------"
+        echo "    $1 - $VERSION"
+        echo "---------------------------------------"
+    else
+        echo "----------------------------------------"
+        echo "git - $VERSION"
+        echo "----------------------------------------"
+
+    fi
+
+    USAGE=$(cat <<- EOF
+	usage:  git options  
+
+	options:
+	  -p path : push to master of path.
+	  -m path ex_file: get modified files of path.
+	  -n path ex_file: get new files of path.
+	EOF
+    )
+    echo "$USAGE"
+    exit
+}
+
+
+
+function git_push_origin_master()
+{
+    if [ $# -lt 1 ]; then
+        echo 'error, pls input path!!!'
+        return
+    fi
+
+    gits=$(find $1 -type d -name .git | sed 's/\/.git//')
+    for git in $gits
+    do
+        echo "-----$git-----"
+        cd $git
+        git push origin master # push to github.
+    done
+    # go back origin path.
+    cd $CURDIR
+}
+
+
+function git_status_files()
+{
+    path=$1
+    if [ $# -gt 2 ]; then
+        ex=$2
+        opt=$3
+    else
+        ex=None
+        opt=$2
+    fi
+    # get all of git.
+    gits=$(find ${path} -type d -name .git | sed 's/\/.git//g')
+    # check all of git.
+    for git in ${gits[@]}
+    do
+        cd ${git}  # switch to git path.
+        fs=$(git status -s | grep ${opt})  # git status -s for status files.
+        if [[ ${#fs} -gt 0 ]]; then  # check status files.
+            if [ $opt == "M" ]; then  # modified files
+                fs=$(echo -e ${fs} | sed 's/M /[Modified]:/g')
+            elif [ $opt == "??" ]; then  # new files
+                fs=$(echo -e ${fs} | sed 's/?? /[New]:/g')
+            fi
+            if [ ${#fs} -gt 0 ]; then
+                pr_path=1
+                for f in ${fs}  # print result.
+                do
+                    # filter exclude files.
+                    if [ $ex != None ]; then
+                        fx=$(echo -e $f | grep -E ${ex}) 
+                        if [ ${#fx} -gt 0 ]; then
+                            continue
+                        fi
+                    fi
+                    # print path.
+                    if [ ${pr_path} == 1 ]; then
+                        echo "-----${git:${#GIT}+1:${#git}}-----"
+                        pr_path=0
+                    fi
+                    # print result file.
+                    echo $f | sed 's/:/: /'
+                done
+            fi
+        fi
+    done
+    # go back origin path.
+    cd $CURDIR
+}
+
+
+if [ $# == 0 ]; then
+    git_help
+else
+    while [ $# -gt 0 ]
+    do
+        case $1 in
+	    -p)
+	        shift
+            if [ $# -gt 0 ]; then
+	            git_push_origin_master $1
+                shift
+            fi
+	        ;;
+	    -m)
+	        shift
+	        git_status_files $1 $2 "M"
+            shift
+            shift
+	        ;;
+	    -n)
+	        shift
+	        git_status_files $1 $2 "??"
+            shift
+            shift
+	        ;;
+	    *)
+            shift
+	        git_help $1
+	    esac
+    done
+fi
--- git/mygit.sh
+++ git/mygit.sh
@@ -1,116 +1,32 @@
 #!/bin/bash
 
-VERSION=1.0.0
+VERSION='1.0.1'
 
-CURDIR=$(pwd)
-
-
-function usage_help()
-{
-    USAGE=$(cat <<- EOF
-	----------------------------------------
-	  mygit - $VERSION
-	----------------------------------------
-	  usage:  mygit options  
-
-	options:
-	  -p | push :
-	    run push origin master.
-	  -m | modified :
-	    check modified files.
-	  -u | unchecked :
-	    check modified files.
-	EOF
-    )
-    echo "$USAGE"
-}
-
-function push_origin_master()
-{
-    for d in $(ls $1)
-    do
-        dr=$1/$d
-        res=$(find $dr -name .git | grep '.git')
-        if [[ -z $res ]]; then
-            continue
-        fi
-        echo "-----$dr-----"
-        cd $dr
-        res=$(git status | grep "modified:")
-        if [[ ! -z $res ]]; then
-            echo "found modified files!!!"
-        else
-            git push origin master # push to github.
-        fi
-    done
-    # go back origin path.
-    cd $CURDIR
-}
-
-
-function check_modified_files()
-{
-    for d in $(ls $1)
-    do
-        dr=$1/$d
-        res=$(find $dr -name .git | grep '.git')
-        if [[ -z $res ]]; then
-            continue
-        fi
-        cd $dr
-        res=$(git status | grep "modified:")
-        if [[ ! -z $res ]]; then
-            echo "-----$dr-----"
-            echo "found modified files!!!"
-        fi
-    done
-    # go back origin path.
-    cd $CURDIR
-}
-
-function check_untracked_files()
-{
-    for d in $(ls $1)
-    do
-        dr=$1/$d
-        res=$(find $dr -name .git | grep '.git')
-        if [[ -z $res ]]; then
-            continue
-        fi
-        cd $dr
-        res=$(git status | grep "Untracked files:")
-        if [[ ! -z $res ]]; then
-            echo "-----$dr-----"
-            echo "found untracked files!!!"
-        fi
-    done
-    # go back origin path.
-    cd $CURDIR
-}
 
 if [ $# == 0 ]; then
-    usage_help
+    mygit_help
     exit -1
 else
+    git_dir=$(cd `dirname $0`; pwd)
     while [ $# -gt 0 ]
     do
         case $1 in
-	    -p | push)
+	    -p)
 	        shift
-	        push_origin_master $GIT
+	        bash ${git_dir}/git.sh -p $GIT
 	        ;;
-	    -m | modified)
+	    -m)
 	        shift
-	        check_modified_files $GIT
+	        bash ${git_dir}/git.sh -m $GIT "pyc|__pycache__"
 	        ;;
-	    -u | unchecked)
+	    -n)
 	        shift
-	        check_untracked_files $GIT
+	        bash ${git_dir}/git.sh -n $GIT "pyc|__pycache__"
 	        ;;
 	    *)
-	        echo "Error, found unknown command $1"
-	        usage_help
-	        exit -1
+            shift
+	        bash ${git_dir}/git.sh -h 'mygit'
+	        exit
 	    esac
     done
 fi
